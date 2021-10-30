eb50c6bdff55cb4d032c8934964decb35f86e13c
LHS11110 <81010297+LHS11110@users.noreply.github.com>
2021-10-31 02:15:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ client = commands.Bot(command_prefix="!", Intents=discord.Intents.all())
 for i in range(len(cogs)):
     cogs[i].setup(client)
 
-client.run('OTA0MDQ4Nzk1MTIwMTk3NjYy.YX13DQ.flgx0lbyhRfvGZbHvK1N-k5ftuI')
+client.run('봇 토큰')
